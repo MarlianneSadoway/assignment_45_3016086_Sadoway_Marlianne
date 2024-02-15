f9c29bb5d94b99f7a6d3c7390f82a575c04a2dbb
MarlianneSadoway <sadowaym7@mymacewan.ca>
2024-02-14 23:21:11 -0700

    deploy to render and Mongodb atlas

--- database/database.js
+++ database/database.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 
 export const connectDB = async () => {
-  const url = `mongodb://localhost:27017/monsters`;
+  const url = `mongodb://localhost:27017`;
   try {
-    const connection = await mongoose.connect(url, {
+    const connection = await mongoose.connect(process.env.DATABASE_URL || url, {
       useUnifiedTopology: true,
     });
     console.log("Database connected successfully");
